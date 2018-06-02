a11fe9277e631b18d90b175f391d51112babf4c1
Ajit Bohra <ajit@lubus.in>
2018-06-02 22:58:00 +0530

    Add plugin pinning

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,11 +6,17 @@ import { PluginSidebar } from "@wordpress/editPost";
 /**
  * Internal dependencies
  */
+import pluginIcon from "../data/icon";
 import Generator from "./generator";
 
 const Sidebar = () => {
   return (
-    <PluginSidebar name="dummynator-sidebar" title="Dummy Content">
+    <PluginSidebar
+      name="dummynator-sidebar"
+      isPinnable={true}
+      icon={pluginIcon}
+      title="Dummy Content"
+    >
       <Generator />
     </PluginSidebar>
   );
